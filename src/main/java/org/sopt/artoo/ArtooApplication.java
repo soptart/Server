caece03ca45a08d7e0c212de33856b44af017e5d
Unknown <winnery93@gmail.com>
2018-12-22 16:46:47 +0900

    Artoo First Commit
    아투 프로젝트 초안

---
+++ src/main/java/org/sopt/artoo/ArtooApplication.java
@@ -0,0 +1,14 @@
+package org.sopt.artoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtooApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArtooApplication.class, args);
+    }
+
+}
+
