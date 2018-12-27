d55bf606d546bea138987b645114f9d7e66e54a9
Unknown <winnery93@gmail.com>
2018-12-27 11:15:12 +0900

    User추가 및 SignUp (Controller 및 Mapper 생성)
    이후 추가 작업 + JWT추가하고 MyPage 쪽 서버 작업 필요

---
+++ src/main/java/org/sopt/artoo/model/UserSignUpReq.java
@@ -0,0 +1,31 @@
+package org.sopt.artoo.model;
+
+import lombok.Data;
+
+@Data
+public class UserSignUpReq {
+    //회원 가입시 입력
+
+    private String u_email;
+    // 유저 ID(이메일)
+    private String u_pw;
+    // 유저 비밀번호
+    private String u_school;
+    // 유저 학교
+    private String u_major;
+    // 유저 전공
+    private int u_phoneNum;
+    // 유저 핸드폰 번호
+    private String u_address;
+    // 유저 주소
+    private String u_name;
+    // 유저 이름
+    private String u_bank;
+    // 은행명
+    private String u_account;
+    // 유저 계좌 번호
+
+    public boolean checkQualification() {
+        return (u_email != null && u_pw != null);
+    }
+}
