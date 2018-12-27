39658dc2a4d7ddd5888c404732b795a826f742e9
DESKTOP-FFLFGB0\ceoah <ceoahn95@gmail.com>
2018-12-28 01:36:10 +0900

    dto Tag추가

---
+++ src/main/java/org/sopt/artoo/dto/Tag.java
@@ -0,0 +1,20 @@
+package org.sopt.artoo.dto;
+
+import lombok.Data;
+
+@Data
+public class Tag {
+    //Tag 고유번호
+    int t_idx;
+    //Tag (ex. 화려한, 조용한)
+    String t_shortTag;
+    //artwork 고유키(외래키)
+    int a_idx;
+    //태크 메인제목
+    String t_mainTag;
+    //태그 서브제목, 눌렀을 때만(t_idx로 받아옴)
+    String t_subTag;
+    //태그 백그라운드 image
+    String t_imgUrl;
+
+}
