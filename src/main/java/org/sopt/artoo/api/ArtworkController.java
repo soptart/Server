f37be3dbdbaa7b317d85543440f36901c12c84b2
jbw1003 <jbw10032@gmail.com>
2018-12-28 14:16:35 +0900

    artworkcontroller like 없이

--- src/main/java/org/sopt/artoo/api/ArtworkController.java
+++ src/main/java/org/sopt/artoo/api/ArtworkController.java
@@ -2,16 +2,16 @@ package org.sopt.artoo.api;
 
 import lombok.extern.slf4j.Slf4j;
 import org.sopt.artoo.dto.Artwork;
+import org.sopt.artoo.model.ArtworkReq;
 import org.sopt.artoo.model.DefaultRes;
 import org.sopt.artoo.service.ArtworkService;
 import org.sopt.artoo.service.JwtService;
 import org.sopt.artoo.utils.ResponseMessage;
 import org.sopt.artoo.utils.StatusCode;
+import org.sopt.artoo.utils.auth.Auth;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -46,9 +46,90 @@ public class ArtworkController {
             DefaultRes<List<Artwork>> defaultRes = artworkService.findAll();
 
             for (Artwork artwork : defaultRes.getData()) {
-
+                artwork.setAuth(userIdx == artwork.getU_idx());
             }
             return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 미술작품 조회
+     *
+     * @param header jwt token
+     * @param a_idx  미술작품 고유 번호
+     * @return ResponseEntity
+     */
+    @GetMapping("/artworks/{a_idx}")
+    public ResponseEntity getArtwork(
+            @RequestHeader(value = "Authorization", required = false) final String header,
+            @PathVariable("a_idx") final int a_idx) {
+        try {
+            final int userIdx = jwtService.decode(header).getUser_idx();
+            DefaultRes<Artwork> defaultRes = artworkService.findByArtIdx(a_idx);
+            defaultRes.getData().setAuth(userIdx == defaultRes.getData().getU_idx());
+            return new ResponseEntity<>(defaultRes, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * 미술작품 작성
+     *
+     * @param header
+     * @param artworkReq
+     * @return ResponseEntity
+     */
+    @Auth
+    @PostMapping("/artworks")
+    public ResponseEntity saveArtwork(
+            @RequestHeader(value = "Authorization") final String header,
+            ArtworkReq artworkReq) {
+        try {
+            artworkReq.setU_idx(jwtService.decode(header).getUser_idx());
+            return new ResponseEntity<>(artworkService.save(artworkReq), HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    /**
+     * @param header     jwt token
+     * @param a_idx      미술작품 인덱스
+     * @param artworkReq 미술작품 데이터
+     * @return
+     */
+    @Auth
+    @PutMapping("/artworks/{a_idx}")
+    public ResponseEntity updateArtwork(
+            @RequestHeader(value = "Authorization") final String header,
+            @PathVariable("a_idx") final int a_idx,
+            ArtworkReq artworkReq) {
+        try {
+            artworkReq.setA_idx(a_idx);
+            if (artworkService.checkAuth(jwtService.decode(header).getUser_idx(), a_idx))
+                return new ResponseEntity<>(artworkService.update(artworkReq), HttpStatus.OK);
+            return new ResponseEntity<>(UNAUTHORIZED_RES, HttpStatus.OK);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
+    @Auth
+    @DeleteMapping("/artworks/{a_idx}")
+    public ResponseEntity deleteArtwork(
+            @RequestHeader(value = "Authorization") final String header,
+            @PathVariable("a_idx") final int a_idx) {
+        try {
+            if(artworkService.checkAuth(jwtService.decode(header).getUser_idx(), a_idx))
+                return new ResponseEntity<>(artworkService.deleteByArtIdx(a_idx), HttpStatus.OK);
+            return new ResponseEntity<>(UNAUTHORIZED_RES,HttpStatus.OK);
         } catch (Exception e) {
             log.error(e.getMessage());
             return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
