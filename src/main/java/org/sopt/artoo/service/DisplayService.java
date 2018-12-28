d34ae552afc75d8e5b1757d0b5d770d5d9fe621a
dayoung9650 <dayoung9650@gmail.com>
2018-12-28 14:46:00 +0900

    merge & display 수정

--- src/main/java/org/sopt/artoo/service/DisplayService.java
+++ src/main/java/org/sopt/artoo/service/DisplayService.java
@@ -13,14 +13,17 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.transaction.interceptor.TransactionAspectSupport;
 
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Date;
+import java.sql.Date;
 import java.util.List;
+
 
 @Slf4j
 @Service
 public class DisplayService {
     private DisplayMapper displayMapper;
+    final static String DAY_DATE_FORMAT = "yyyy-MM-dd";
 
     public DisplayService(DisplayMapper displayMapper) {
         this.displayMapper = displayMapper;
@@ -33,35 +36,49 @@ public class DisplayService {
      */
 
     public DefaultRes<List<Display>> findDisplays(){
-        String month = getMonth();
+//        String month = getMonth();
+//
+//        List<Display> displayList = displayMapper.findNow(month);
+//        List<Display> displayListApp = displayMapper.findApp(month);
+//
+//        for(Display display : displayList) { display.setIsNow(1); }
+//        for(Display display : displayListApp) { display.setIsNow(0); }
+//
+//        displayList.addAll(displayListApp);
+//        if(displayList == null || displayListApp == null)
+//            return DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_DISPLAY);
+//        return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_ALL_DISPLAY, displayList);
 
-        List<Display> displayList = displayMapper.findNow(month);
-        List<Display> displayListApp = displayMapper.findApp(month);
+        java.util.Date date = new java.util.Date();
+        Calendar now = Calendar.getInstance();
+        now.setTime(date);
+        log.info(now.toString());
 
-        for(Display display : displayList) { display.setIsNow(1); }
-        for(Display display : displayListApp) { display.setIsNow(0); }
+        List<Display> displayList = displayMapper.findAllDisplay();
 
-        displayList.addAll(displayListApp);
-        if(displayList == null || displayListApp == null)
+        for(Display display : displayList){
+            Calendar sCalApp = Calendar.getInstance();
+            Calendar eCalApp = Calendar.getInstance();
+            sCalApp.setTime(Date.valueOf(display.getD_sdateApp()));
+            eCalApp.setTime(Date.valueOf(display.getD_edateApp()));
+
+            Calendar sCalNow = Calendar.getInstance();
+            Calendar eCalNow = Calendar.getInstance();
+            sCalNow.setTime(Date.valueOf(display.getD_sdateNow()));
+            eCalNow.setTime(Date.valueOf(display.getD_edateNow()));
+
+            //신청 중
+            if(now.compareTo(sCalApp) != -1 && now.compareTo(eCalApp)  != 1){ display.setIsNow(0);}
+            //전시 중
+            if(now.compareTo(sCalNow) != -1 && now.compareTo(eCalNow)  != 1){ display.setIsNow(1); }
+        }
+        if(displayList == null)
             return DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_DISPLAY);
         return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_ALL_DISPLAY, displayList);
-
-//        java.util.Date date = new Date();
-//        Calendar now = Calendar.getInstance();
-//
-//        List<Display> displayList = displayMapper.findAllDisplay();
-//
-//        for(Display display : displayList){
-//            Calendar cal = Calendar.getInstance();
-//            cal.setTime(display.getD_sdateApp());
-//
-//            if(display.getD_sdateApp().compareTo(now)
-//            display.getD_sdateApp()
-//        }
     }
 
     public String getMonth() {
-        java.util.Date date = new Date();
+        java.util.Date date = new java.util.Date();
         Calendar cal = Calendar.getInstance();
         cal.setTime(date);
         int month = cal.get(Calendar.MONTH);
@@ -82,6 +99,4 @@ public class DisplayService {
             return DefaultRes.res(StatusCode.NOT_FOUND, ResponseMessage.NOT_FOUND_DISPLAY);
         return DefaultRes.res(StatusCode.OK, ResponseMessage.READ_DISPLAY, display);
     }
-
 }
-
