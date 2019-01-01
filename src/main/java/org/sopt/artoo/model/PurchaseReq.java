5426830d913f15c04694d5586b3faaacd1d58f38
jbw1003 <jbw10032@gmail.com>
2019-01-01 17:56:08 +0900

    purchase 자기가 판매/구매한 내역 조회

--- src/main/java/org/sopt/artoo/model/PurchaseReq.java
+++ src/main/java/org/sopt/artoo/model/PurchaseReq.java
@@ -3,25 +3,29 @@ package org.sopt.artoo.model;
 
 import lombok.*;
 
+import java.util.Date;
+
 @Data
 public class PurchaseReq {
+    // 구매 인덱스
+    private int p_idx;
+    // 구매 상태
     private int p_state;
-    //주문 상태 (주문 방법)
+    // 구매 날짜
+    private Date p_date;
+    // 구매 후기
+    private String p_comment;
+    // 작품 고유 인덱스
     private int a_idx;
-    //작품 고유 번호
-    private int u_idx;
-    //구매자 고유 번호
+    // 작품 구매자 인덱스
+    private int p_buyer_idx;
+    // 작품 판매자 인덱스
+    private int p_seller_idx;
+    // 작품 수령자 이름
+    private String p_recipient;
+    // 작품 수령 주소
+    private String p_address;
+    // 구매자 / 판매자 구분
+    private boolean p_isBuyer;
 
-
-    public PurchaseReq() {
-        this.p_state = -1;
-        this.a_idx = -1;
-        this.u_idx = -1;
-    }
-
-    public boolean checkPurchaseReq() {
-        return (
-                p_state != -1 && a_idx != -1 && u_idx != -1
-        );
-    }
 }
