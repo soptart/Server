28b5ab3e155301aed37e097a61592ea845364bab
jbw1003 <jbw10032@gmail.com>
2019-01-08 12:19:12 +0900

    ..

--- src/main/java/org/sopt/artoo/api/CommentController.java
+++ src/main/java/org/sopt/artoo/api/CommentController.java
@@ -42,16 +42,11 @@ public class CommentController {
             //토큰으로 유저인덱스 가져오기
             final int userIdx = jwtService.decode(header).getUser_idx();
             // userIdx로 유저가 쓴 코멘트들을 가져와서 그 코멘트 중에 이 artwork에 artwork가 가지고 있는 c_idx를 가진 코멘트가 있는지 확인
-            DefaultRes<List<Comment>> commentList = commentService.findAllCommentByArtIdx(a_idx);
-            if (commentList.getData().stream().filter(comment -> comment.getU_idx() == userIdx).collect(Collectors.toList()).size() != 0) {
-                for (Comment comment : commentList.getData()) {
-                    comment.setAuth(userIdx == comment.getU_idx());
-                }
-                return new ResponseEntity<>(commentList, HttpStatus.OK);
-            } else {
-                List<Comment> emptyList = new ArrayList<>();
-                return new ResponseEntity<>(DefaultRes.res(StatusCode.NO_CONTENT, ResponseMessage.INDEX_NOT_FOUNDED, emptyList), HttpStatus.OK);
+            if (userIdx == -1) {
+                return new ResponseEntity<>(DefaultRes.res(StatusCode.UNAUTHORIZED, ResponseMessage.INDEX_NOT_FOUNDED), HttpStatus.OK);
             }
+            DefaultRes<List<Comment>> commentList = commentService.findAllCommentByArtIdx(a_idx, userIdx);
+            return new ResponseEntity<>(commentList, HttpStatus.OK);
         } catch (Exception e) {
             log.error(e.getMessage());
             return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
