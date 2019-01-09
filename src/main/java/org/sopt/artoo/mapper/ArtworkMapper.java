c73890759218ce4b6c594f496ec07f0e94a0b76e
jbw1003 <jbw10032@gmail.com>
2019-01-10 01:58:44 +0900

    artworkmapper 좋아요 수 수정 안 되게 바꿈

--- src/main/java/org/sopt/artoo/mapper/ArtworkMapper.java
+++ src/main/java/org/sopt/artoo/mapper/ArtworkMapper.java
@@ -66,10 +66,10 @@ public interface ArtworkMapper {
     @Options(useGeneratedKeys = true, keyProperty = "artworkReq.a_idx")
     void save(@Param("artworkReq") final ArtworkReq artworkReq);
 
-    @Update("UPDATE artwork SET a_name = #{artworkReq.a_name}, a_width = #{artworkReq.a_width}, a_height = #{artworkReq.a_height}, a_depth = #{artworkReq.a_depth}, a_category = #{artworkReq.a_category}, a_form = #{artworkReq.a_form}, a_price = #{artworkReq.a_price}, a_like_count = #{artworkReq.a_like_count}, a_detail = #{artworkReq.a_detail}, a_date = #{artworkReq.a_date}, a_year = #{artworkReq.a_year}, a_active = #{artworkReq.a_active}, a_tags = #{artworkReq.a_tags}, a_license = #{artworkReq.a_license} WHERE a_idx = #{a_idx}")
+    @Update("UPDATE artwork SET a_name = #{artworkReq.a_name}, a_width = #{artworkReq.a_width}, a_height = #{artworkReq.a_height}, a_depth = #{artworkReq.a_depth}, a_category = #{artworkReq.a_category}, a_form = #{artworkReq.a_form}, a_price = #{artworkReq.a_price}, a_detail = #{artworkReq.a_detail}, a_date = #{artworkReq.a_date}, a_year = #{artworkReq.a_year}, a_active = #{artworkReq.a_active}, a_tags = #{artworkReq.a_tags}, a_license = #{artworkReq.a_license} WHERE a_idx = #{a_idx}")
     void updateByArtIdx(@Param("artworkReq") final Artwork artworkReq, @Param("a_idx") final int a_idx);
 
-    @Update("UPDATE artwork SET a_name = #{artworkReq.a_name}, a_width = #{artworkReq.a_width}, a_height = #{artworkReq.a_height}, a_depth = #{artworkReq.a_depth}, a_category = #{artworkReq.a_category}, a_form = #{artworkReq.a_form}, a_price = #{artworkReq.a_price}, a_like_count = #{artworkReq.a_like_count}, a_detail = #{artworkReq.a_detail}, a_date = #{artworkReq.a_date}, a_year = #{artworkReq.a_year}, a_active = #{artworkReq.a_active}, a_tags = #{artworkReq.a_tags}, a_license = #{artworkReq.a_license} WHERE a_idx = #{a_idx}")
+    @Update("UPDATE artwork SET a_name = #{artworkReq.a_name}, a_width = #{artworkReq.a_width}, a_height = #{artworkReq.a_height}, a_depth = #{artworkReq.a_depth}, a_category = #{artworkReq.a_category}, a_form = #{artworkReq.a_form}, a_price = #{artworkReq.a_price}, a_detail = #{artworkReq.a_detail}, a_date = #{artworkReq.a_date}, a_year = #{artworkReq.a_year}, a_active = #{artworkReq.a_active}, a_tags = #{artworkReq.a_tags}, a_license = #{artworkReq.a_license} WHERE a_idx = #{a_idx}")
     void updateByArtIdxReq(@Param("artworkReq") final ArtworkReq artworkReq, @Param("a_idx") final int a_idx);
 
     @Delete("DELETE FROM artwork WHERE a_idx = #{a_idx}")
