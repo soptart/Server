a56d02e88cc0adf03723f86f9ea5f23277441c2b
dayoung9650 <dayoung9650@gmail.com>
2019-02-22 15:55:53 +0900

    reset 중인데 이게 뭘까 망한거같은데

--- src/main/java/org/sopt/artoo/api/UserController.java
+++ src/main/java/org/sopt/artoo/api/UserController.java
@@ -1,11 +1,12 @@
 package org.sopt.artoo.api;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import lombok.extern.slf4j.Slf4j;
 import org.sopt.artoo.dto.MyPageRes;
-import org.sopt.artoo.model.*;
+import org.sopt.artoo.model.DefaultRes;
+import org.sopt.artoo.model.UserDescriptionReq;
+import org.sopt.artoo.model.UserPwInfo;
+import org.sopt.artoo.model.UserSignUpReq;
 import org.sopt.artoo.service.JwtService;
-import org.sopt.artoo.service.KakaoService;
 import org.sopt.artoo.service.UserService;
 import org.sopt.artoo.utils.auth.Auth;
 import org.springframework.http.HttpStatus;
@@ -14,7 +15,6 @@ import org.springframework.web.bind.annotation.*;
 
 import static org.sopt.artoo.model.DefaultRes.FAIL_AUTHORIZATION_RES;
 import static org.sopt.artoo.model.DefaultRes.FAIL_DEFAULT_RES;
-import static org.sopt.artoo.model.DefaultRes.FAIL_FIND_USER;
 
 @RestController
 @RequestMapping("/users")
@@ -23,19 +23,12 @@ public class UserController {
 
     private final UserService userService;
     private final JwtService jwtService;
-    private final KakaoService kakaoService;
 
-    public UserController(final UserService userService, final JwtService jwtService, final KakaoService kakaoService){
+    public UserController(final UserService userService, final JwtService jwtService){
         this.userService = userService;
         this.jwtService = jwtService;
-        this.kakaoService = kakaoService;
     }
 
-    /**
-     * 이메일 중복 검사
-     * @param email
-     * @return
-     */
     @PostMapping("/u_email/{u_email}")
     public ResponseEntity getUserEmail(
             @PathVariable("u_email") final String email){
@@ -138,49 +131,6 @@ public class UserController {
     }
 
     /**
-     * 외부 회원 가입입니다.
-     * @param userSignUpReq
-     * @return
-     */
-    @PostMapping("/external")
-    public ResponseEntity userSignUpExternal(@RequestBody final UserSignUpReq userSignUpReq){
-        if(userSignUpReq.getU_type() == UserSignUpReq.Kakao) {
-            JsonNode userInfo = kakaoService.verifyAccessToken(userSignUpReq.getAccessToken());
-            //우선은 id로 해놨는데 나중에 email로 바꿔도 되는건지 확인해봅시당
-            if (!userInfo.path("id").isMissingNode()) {
-                userSignUpReq.setExternal_key(userInfo.path("id").asInt());
-                if (userService.findByUserIdAndType(userSignUpReq.getExternal_key(), userSignUpReq.getU_type())) {
-                    return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.BAD_REQUEST);
-                }
-                JsonNode kakao_account = userInfo.path("kakao_account");
-                if (kakao_account.path("has_email").asBoolean() && kakao_account.path("is_email_valid").asBoolean() && kakao_account.path("is_email_verified").asBoolean()) {
-                    userSignUpReq.setU_email(kakao_account.path("email").asText());
-                }
-                JsonNode properties = userInfo.path("properties");
-                if(properties.path("nickname").asText()!=null){
-                    userSignUpReq.setU_name(userInfo.path("nickname").asText());
-                    log.info("nickname: "+userInfo.path("nickname").asText());
-                }else{
-                    userSignUpReq.setU_name("no_name");
-                    log.info("no_name");
-                }
-            } else {
-                return new ResponseEntity<>(FAIL_FIND_USER, HttpStatus.BAD_REQUEST);
-            }
-            try {
-                userSignUpReq.setU_type(UserSignUpReq.Kakao);
-                return new ResponseEntity<>(userService.saveExternal(userSignUpReq), HttpStatus.OK);
-            } catch (Exception e) {
-                e.printStackTrace();
-                log.info("failed set u_type");
-                return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }else{
-            return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.BAD_REQUEST);
-        }
-    }
-
-    /**
      * 유저 소개란 삽입/수정
      *
      * @RequestBody userDescription
@@ -211,7 +161,7 @@ public class UserController {
 
 
     /**
-     * 유저정보조회
+     *
      * @param userIdx
      * @return User 객체
      */
@@ -276,32 +226,6 @@ public class UserController {
         } else {
             return new ResponseEntity(FAIL_AUTHORIZATION_RES, HttpStatus.UNAUTHORIZED);
         }
-    }
-
-
-    /**
-     * 유저 삭제
-     * @param header
-     * @param userPw
-     * @param userIdx
-     * @return defaultRes
-     */
-    @DeleteMapping("/{u_idx}")
-    public ResponseEntity deleteUser(
-            @RequestHeader (value = "Authorization", required =  false) final String header,
-            @RequestBody final LoginReq userPw,
-            @PathVariable("u_idx") final int userIdx) {
-        if(jwtService.checkAuth(header, userIdx)){
-            try{
-                DefaultRes defaultRes = userService.deleteUser(userIdx, userPw);
-                return new ResponseEntity<>(defaultRes, HttpStatus.OK);
-            } catch (Exception e) {
-                log.error(e.getMessage());
-               return new ResponseEntity<>(FAIL_DEFAULT_RES, HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-        return new ResponseEntity(FAIL_AUTHORIZATION_RES, HttpStatus.UNAUTHORIZED);
-
     }
 }
 
--- src/main/java/org/sopt/artoo/model/UserSignUpReq.java
+++ src/main/java/org/sopt/artoo/model/UserSignUpReq.java
@@ -4,9 +4,6 @@ import lombok.Data;
 
 @Data
 public class UserSignUpReq {
-    public final static int original = 0;
-    public final static int Kakao = 1;
-    public final static int Naver = 2;
     //회원 가입시 입력
 
     private String u_email;
@@ -17,16 +14,18 @@ public class UserSignUpReq {
     // 유저 학교
     private String u_phone;
     // 유저 핸드폰 번호
+    private String u_address;
+    // 유저 주소
     private String u_name;
     // 유저 이름
+    private String u_bank ="";
+    // 은행명
+    private String u_account;
+    // 유저 계좌 번호
+    private String u_dept;
+    //유저 전공
     private String u_description;
     // 유저 정보 변경
-    private String accessToken;
-    // 카카오 rest api key
-    // 유저 로그인 타입
-    private int u_type;
-    // 외부 인덱스
-    private int external_key;
 
     public boolean checkQualification() {
         return (u_email != null && u_pw != null && u_phone != null && u_name != null);
